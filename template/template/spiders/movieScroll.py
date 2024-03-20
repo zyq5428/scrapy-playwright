7724d7749c43044b94139ac221a31b16af2a9476
zyq5428 <594270026@qq.com>
2024-03-20 18:50:13 +0800

    实现翻页，但提取文本有问题

--- template/template/spiders/movieScroll.py
+++ template/template/spiders/movieScroll.py
@@ -4,6 +4,7 @@ import json
 from urllib.parse import urljoin
 from template.items import MovieScrollItem
 from scrapy_playwright.page import PageMethod
+from pyquery import PyQuery as pq
 
 BASE_URL = 'https://spa3.scrape.center/'
 
@@ -37,10 +38,19 @@ class MoviescrollSpider(scrapy.Spider):
 
     async def parse_index(self, response):
         page = response.meta['playwright_page']
-        await page.mouse.wheel(0, 100)
-        card = page.locator("div.el-card:nth-child(100)")
-        await card.wait_for()
+        pre_name = ''
+        cur_name = await page.locator('.el-card .m-b-sm').nth(-1).text_content()
+        while (cur_name != pre_name):
+            await page.evaluate('window.scrollTo(0, document.body.scrollHeight)')
+            await page.mouse.wheel(0, 10000)
+            await page.wait_for_timeout(3000)
+            pre_name = cur_name
+            cur_name = await page.locator('.el-card .m-b-sm').nth(-1).text_content()
+        # card = page.locator("div.el-card:nth-child(100)")
+        # await card.wait_for()
         screenshot = await page.screenshot(path="./image/" + "web.png", full_page=True)
+        name_list = response.css('.el-card .m-b-sm::text').getall()
+        print('name list is ', name_list)
 
     async def errback_close_page(self, failure):
         page = failure.request.meta['playwright_page']
